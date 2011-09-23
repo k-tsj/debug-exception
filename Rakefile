fd693a72a0c2dd5960c2884ffb8260e7f2534511
Kazuki Tsujimoto <kazuki@callcc.net>
2011-09-23 13:00:09 +0900

    Add tests.

--- Rakefile
+++ Rakefile
@@ -5,3 +5,11 @@ require "bundler/gem_tasks"
 require "rake/extensiontask"
 Rake::ExtensionTask.new("debug_exception", DebugExceptionSpec) do |ext|
 end
+
+require "rake/testtask"
+task :default => :test
+task :test => :compile
+Rake::TestTask.new do |t|
+  t.libs << "lib"
+  t.test_files = FileList["test/test_*.rb"]
+end
---
+++ test/test_debug-exception.rb
@@ -0,0 +1,29 @@
+require 'test/unit'
+require 'pty'
+require 'timeout'
+
+class DebugExceptionTest < Test::Unit::TestCase
+  RUBY = RbConfig.ruby
+  LIB = File.expand_path("../../lib", __FILE__)
+
+  def test_start_repl_on_exception
+    timeout(2) {
+      begin
+        r, w, pid = PTY.spawn(RUBY, '-I', LIB, '-rdebug-exception',
+                        '-e', 'def f(i); raise "test"; end; f(999)')
+        assert_match(/RuntimeError/, r.gets)
+        assert_match(/from -e/, r.gets)
+        assert_match(/from -e/, r.gets)
+        assert_match(/irb/, r.readpartial(20))
+        w.puts 'i'
+        assert_match(/999/, r.readpartial(20))
+        w.puts 'exit'
+      ensure
+        if pid
+          _, stat = Process.wait2(pid)
+          assert_equal(true, stat.exited?)
+        end
+      end
+    }
+  end
+end
